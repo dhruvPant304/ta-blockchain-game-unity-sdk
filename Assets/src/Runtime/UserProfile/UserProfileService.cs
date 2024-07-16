e3a0573d030c05e2dd75a356967e82080058b4f5
DhruvPant <dhruv304c2@gmail.com>
2024-07-16 13:51:01 +0530

    added session saving and loading methods in user profile service

--- Assets/src/Runtime/UserProfile/UserProfileService.cs
+++ Assets/src/Runtime/UserProfile/UserProfileService.cs
@@ -6,6 +6,7 @@ using TA.Authentication;
 using TA.Helpers.Crypto;
 using TA.Services;
 using UnityEngine;
+using Newtonsoft.Json;
 
 namespace TA.UserProfile{
 public class UserProfileService : Service<UserProfileService>{
@@ -18,21 +19,21 @@ public class UserProfileService : Service<UserProfileService>{
     string platform = "android";
     string appType = "etuktuk";
 
-    LoginUserData _userData;
-    public LoginUserData UserData => _userData;
+    LoginSessionData _userData;
+    public LoginSessionData UserData => _userData;
 
     UserBalanceData _userBalanceData;
     public UserBalanceData UserBalanceData => _userBalanceData;
 
     //Events
-    public Action<LoginUserData> OnAuthSuccess;
+    public Action<LoginSessionData> OnAuthSuccess;
     public Action<FailedResponse>  OnAuthFailed;
     public Action OnAuthComplete;
     public Action<UserBalanceData> OnBalanceUpdate;
     public Action OnBalanceUpdateFailed;
 
-    private bool _loggedIn;
-    public bool LoggedIn => _loggedIn;
+    public bool LoggedIn {get; private set;}
+    public bool IsAutoLoginSession {get; private set;}
 
     void Start(){
         _web3AuthService = ServiceLocator.Instance.GetService<Web3AuthService>();
@@ -64,20 +65,21 @@ public class UserProfileService : Service<UserProfileService>{
         var requestResponse = await _apiService.SendLoginRequest(loginParams);
 
         if(requestResponse.IsSuccess) 
-            HandleAuthSuccess(requestResponse.SuccessResponse);
+            HandleAuthSuccess(requestResponse.SuccessResponse.data);
         else 
             HandleAuthFailiure(requestResponse.FailureResponse);
 
-        _loggedIn = true;
         OnAuthComplete?.Invoke();
     }
 
-    void HandleAuthSuccess(LoginResponse response){
-         var loginData = response.data;
+    void HandleAuthSuccess(LoginSessionData response){
+         var loginData = response;
         _userData = loginData;
 
         OnAuthSuccess?.Invoke(_userData);
         UpdateUserBalance().Forget();
+
+        LoggedIn = true;
     }
 
     void HandleAuthFailiure(FailedResponse failedResponse){
@@ -95,5 +97,27 @@ public class UserProfileService : Service<UserProfileService>{
         }
         return response;
     }
+
+    public const string LOGIN_SESSION_KEY = "LOGIN_SESSION_KEY";
+
+    public void StartSavedLoginSession(){
+        var loginData = JsonConvert.DeserializeObject<LoginSessionData>(PlayerPrefs.GetString(LOGIN_SESSION_KEY));
+        HandleAuthSuccess(loginData);
+    }
+
+    void SaveLoginSession(LoginSessionData loginData){
+        PlayerPrefs.SetString(LOGIN_SESSION_KEY, JsonUtility.ToJson(loginData));
+        PlayerPrefs.Save();
+    }
+
+    void ClearLoginSession(){
+        PlayerPrefs.DeleteKey(LOGIN_SESSION_KEY);
+        PlayerPrefs.Save();
+    }
+
+    bool HasSavedLoginSession(){
+        return PlayerPrefs.HasKey(LOGIN_SESSION_KEY);
+    }
 }
+
 }
