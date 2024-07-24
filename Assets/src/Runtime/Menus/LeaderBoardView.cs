96746dd0c13024d254d5a73a897437923bb8cc69
DhruvPant <dhruv304c2@gmail.com>
2024-07-24 19:29:04 +0530

    fix: clearing options in order to prevent them from being re-added to dropdown

--- Assets/src/Runtime/Menus/LeaderBoardView.cs
+++ Assets/src/Runtime/Menus/LeaderBoardView.cs
@@ -66,6 +66,7 @@ public class LeaderBoardView : MonoBehaviour {
                         text = name
                     });
         }
+        leaderBoardNameDropdown.ClearOptions();
         leaderBoardNameDropdown.AddOptions(options);
     }
 
