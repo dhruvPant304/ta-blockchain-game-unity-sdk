c76511d1f095fcdb89f6f44dff1272aabf5284f9
DhruvPant <dhruv304c2@gmail.com>
2024-07-25 17:09:15 +0530

    removing instantiated bool on singleton

--- Assets/src/Runtime/Singleton/Singleton.cs
+++ Assets/src/Runtime/Singleton/Singleton.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 namespace TA{
 public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>{
 	static T _instance;
-	public static bool instantiated;
 	public static T Instance { get {
 		if(_instance == null){
 			_instance = GameObject.FindObjectOfType<T>();	
 		}			
-		instantiated = true;
 		return _instance;	
 	}}
 
