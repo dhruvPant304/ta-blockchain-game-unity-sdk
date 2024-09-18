069dd5b501601e5ca6b57f776388b152ee623614
DhruvPant <dhruv304c2@gmail.com>
2024-09-18 14:58:58 +0530

    fix: not showing free credits pop-up if game is free to play

--- Assets/src/Runtime/ExceptionHandlers/AuthExceptionHandler.cs
+++ Assets/src/Runtime/ExceptionHandlers/AuthExceptionHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TA.APIClient;
 using TA.APIClient.ResponseData;
 using TA.Authentication;
 using TA.Components;
@@ -13,11 +14,13 @@ public class AuthExceptionHandler : MonoBehaviour{
     UserProfileService _profileService;
     Web3AuthService _web3AuthService;
     BlockchainGameCanvas _blockChainGameCanvas;
+    APIConfig _apiConfig;
 
     async void Start(){
         _profileService = ServiceLocator.Instance.GetService<UserProfileService>();
         _web3AuthService = ServiceLocator.Instance.GetService<Web3AuthService>();
         _blockChainGameCanvas = ServiceLocator.Instance.GetService<BlockchainGameCanvas>();
+        _apiConfig = ServiceLocator.Instance.GetService<APIConfigProviderService>().APIConfig;
         
         _profileService.OnAuthFailed += OnAuthFailed;
         _profileService.OnAuthSuccess += OnAuthSuccess;
@@ -52,8 +55,10 @@ public class AuthExceptionHandler : MonoBehaviour{
     }
 
     void OnAuthSuccess(LoginSessionData userData){
-        if(!userData.isFirstTimeUser || _profileService.IsAutoLoginSession) {
-            Debug.Log("not a first time user");
+        if(!userData.isFirstTimeUser 
+                || _profileService.IsAutoLoginSession 
+                || !_apiConfig.requireCreditsToPlay) {
+            if(!userData.isFirstTimeUser) Debug.Log("not a first time user");
             SceneManager.LoadScene(3);
             _profileService.OnAuthSuccess -= OnAuthSuccess;
             return;
