b39ee82d4e647b4dc49af111802402a78ba06963
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2024-10-28 12:39:57 +0530

    Revert "updating how"
    This reverts commit ab7f3deb6a6f5c39fc9dbfcb56ed6d9e5c818417.

--- Assets/ETukTukSamples/UpdateLoginTextForScene.cs
+++ Assets/ETukTukSamples/UpdateLoginTextForScene.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,19 +5,12 @@ using UnityEngine.SceneManagement;
 public class UpdateLoginTextForScene : MonoBehaviour{
     [SerializeField] TextMeshProUGUI textMesh;
 
-    void OnEnable()
-    {
+    void OnEnable(){
         var sceneName = SceneManager.GetActiveScene().name;
 
-        // Set the initial text to "Login"
         textMesh.text = "Login";
-
-        // If the scene name is "SignIn", perform replacements accordingly
-        if (sceneName.Equals("SignIn", StringComparison.OrdinalIgnoreCase))
-        {
-            // Swap "Login" with "Sign In" and vice versa in a case-insensitive manner
-            textMesh.text = Regex.Replace(textMesh.text, @"(?i)login", "Sign In", RegexOptions.IgnoreCase);
-            textMesh.text = Regex.Replace(textMesh.text, @"(?i)sign in", "Login", RegexOptions.IgnoreCase);
+        if(sceneName == "SignIn"){
+            textMesh.text = "Sign In"; 
         }
     }
 }
