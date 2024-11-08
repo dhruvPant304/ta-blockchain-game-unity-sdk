f0d67ab4ce2bc8e58e1a3ff1ce817186dc282016
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2024-11-08 20:45:29 +0530

    more information items and copy text button

--- Assets/src/Runtime/Debug/InfoPanel.cs
+++ Assets/src/Runtime/Debug/InfoPanel.cs
@@ -2,31 +2,39 @@ using UnityEngine;
 using TMPro;
 using TA.Services;
 using TA.APIClient;
+using TA.Authentication;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 namespace TA.TADebug{
     public class InfoPanel : MonoBehaviour {
         [SerializeField] TextMeshProUGUI infoText;
+        [SerializeField] Button copyButton;
 
         void Start(){
-            var info = $"Env: {Environment()},";
-            info += $" BuildVersion: {Application.version}";
+            infoText.overflowMode = TextOverflowModes.ScrollRect;
+
+            var apiConfig = ServiceLocator.Instance.GetService<APIConfigProviderService>().APIConfig;
+            var web3AuthConfig = ServiceLocator.Instance.GetService<Web3AuthConfigProviderService>().Config;
+
+            var infoDict = new Dictionary<string,string>(){
+                {"Env", apiConfig.environment.ToString()},
+                {"BuildVersion", Application.version},
+                {"API Url", apiConfig.serverUrl},
+                {"Web3Auth ClientId", web3AuthConfig.clientID},
+                {"Web3Auth Network", web3AuthConfig.network.ToString()}
+            };
+
+            var info = "";
+            foreach(var pair in infoDict){
+                info += $"{pair.Key} : {pair.Value}, ";
+            }
+
             infoText.text = info;
+
+            copyButton.onClick.AddListener(() => GUIUtility.systemCopyBuffer = info);
         }
 
-        string Environment(){
-            var env = ServiceLocator.Instance.GetService<APIConfigProviderService>().APIConfig.environment;
-            switch(env){
-                case APIClient.Environment.Development:
-                    return "Development";
-                case APIClient.Environment.Testing:
-                    return "Testing";
-                case APIClient.Environment.Staging:
-                    return "Staging";
-                case APIClient.Environment.Production:
-                    return "Production";
-            }
-            return "";
-        }
     }
 }
 
