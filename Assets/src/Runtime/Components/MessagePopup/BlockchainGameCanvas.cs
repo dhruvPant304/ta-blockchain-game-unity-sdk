dbf10a3d2ea9c4574e07c6f8cb9b8b78cde41c94
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2024-11-12 11:52:07 +0530

    stacking message pop up requests until the active popup is not hidden

--- Assets/src/Runtime/Components/MessagePopup/BlockchainGameCanvas.cs
+++ Assets/src/Runtime/Components/MessagePopup/BlockchainGameCanvas.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using TA.Services;
 using UnityEngine;
 
@@ -8,8 +10,29 @@ public class BlockchainGameCanvas : Service<BlockchainGameCanvas>{
     protected override void OnInitialize(){
     }
 
+    void Start(){
+        StartCoroutine(ProcessRequestStack().ToCoroutine());
+    }
+
+    private Stack<PopUpRequest> requests;
+
     public void ShowMessagePopup(MessagePopup messagePopup, int styleIndex = 0){
-        presenter.ShowMessagePopup(messagePopup, styleIndex);
+        requests.Push(new PopUpRequest{popup = messagePopup, styleIndex = styleIndex});
+    }
+
+    public async UniTask ProcessRequestStack(){
+        while(true){
+            await UniTask.WaitWhile(() => requests.Count == 0);
+            var req = requests.Peek();
+            presenter.ShowMessagePopup(req.popup, req.styleIndex);
+            await UniTask.WaitUntil(() => presenter.IsHidden);
+            requests.Pop();
+        }
+    }
+
+    public struct PopUpRequest{
+        public MessagePopup popup;
+        public int styleIndex;
     }
 }
 }
